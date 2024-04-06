48f073d27eca0ab186238f7beb0f6f9701705e10
sincera <spzspz@live.com>
2024-04-06 10:39:48 +0900

    perf: 문제 답할때 다음 문제로 넘어가도록 로직 수정

--- src/component/Question.jsx
+++ src/component/Question.jsx
@@ -2,47 +2,38 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Question = (props) => {
-  const [started, setStarted] = useState(true);
   const [selectedOption, setSelectedOption] = useState(null);
-  const [question, setQuestion] = useState([]);
-  const [questionNum , setQuestionNum] = useState(0);
-
+  const [questionArr, setQuestionArr] = useState([]);
+  const [currentQuestion , setCurrentQuestion] = useState(null);
+  const [questionIndex , setQuestionIndex] = useState(0);
   useEffect(()=>{
     axios.get("/json/question2.json")
-   //   axios.get("/api/server_test")  
          .then((res)=>{
-        ///  const response = JSON.parse(res.data);
-          setQuestion(res.data);
-          console.dir(res);
+          setQuestionArr(res.data);
+          setCurrentQuestion(res.data[0]);
          })
   },[])
-  // 시작 버튼을 클릭할 때 호출되는 함수
-  const handleStartButtonClick = () => {
-    setStarted(true);
-  };
 
-  // 선택한 옵션을 처리하는 함수
-  const handleOptionSelect = (option) => {
-    setSelectedOption(option);
+  const moveNextQuestion = (option) => {
+    setQuestionIndex(questionIndex+1);
+    setCurrentQuestion(questionArr[questionIndex+1]);
   };
 
   return (
-    <>
-    {question?
-      question.map((list,index)=>(
+  
         <div>
-          <h2>{list.question}</h2>
-          {list.options?
-          list.options.map((option) =>(  
-          <button onClick={() => handleOptionSelect(option.value)}>{option.text}</button>
-           ))
-           :<h2>데이터 통신 오류</h2>
-          }
+           {currentQuestion &&(
+            <h2>{currentQuestion.question}</h2>             
+            )
+          }  
+          {currentQuestion &&(
+            currentQuestion.options.map((list,index) => (
+              <button key={list.value} onClick={() => moveNextQuestion(list.value)}>{list.text}</button>
+              )
+            )
+          )}
         </div>
-         
-      ))
-    :<h2>데이터 통신 오류</h2>}  
-    </>
+
   );
 }
 
