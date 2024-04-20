e7a6fecac3e528e5075c3d42b94426bcce34dfef
sincera <spzspz@live.com>
2024-04-20 23:25:19 +0900

    REFACTOR : 메인 렌더링 개선

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -6,11 +6,15 @@ import axios from 'axios';
 const Main = (props) => {
 
   const [resultMember, setResultMember] = useState(0);
+  const [data , setData] = useState(false);
 
   useEffect(()=>{
     axios.post("/holymbti/getResultMember")
          .then((res)=>{
           setResultMember(res.data);
+          setData(true);
+         }).catch((error)=>{
+          setData(false);
          })
   },[])
 
@@ -18,13 +22,15 @@ const Main = (props) => {
     props.pathHandler("start");
   }
   return (
+
     <div className="App">
+      {data?
          <div className="start-page">
           <h2>전신갑주 입고 홀스 성회 가자!</h2>
           <p>나에게 필요한 말씀의 검은?</p>
           <button className="button" onClick={moveStart}>시작하기<br/><p>총 {resultMember}명 참여하였습니다.</p></button>
-      
         </div>
+    :null}
     </div>
   );
 }
