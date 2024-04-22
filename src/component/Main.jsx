36045798cf9746704a68b296d3005235e910f1bd
sincera <spzspz@live.com>
2024-04-22 21:50:16 +0900

    REFACTOR : 메인 렌더링 개선

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -25,23 +25,25 @@ const Main = (props) => {
   }
   return (
   <div className="container">
-  <section id="main" className="mx-auto mt-2 py-5 px-5">
-    <h3 className="mx-auto mb-4">전신갑주 입고 홀스 성회 가자</h3>
-    <div className="col-5 mx-auto col-md-5 col-sm-5 col-12">
-      <img  src={require(`../images/main2.png`)} className="img-fluid" />
-    </div>
-    <p className="mt-3 mb-4">나에게 맞은 말씀의 검은?</p>
-    <button onClick={moveStart}>시작하기</button>
-    <div className="logo-container">
-      <img
-        src={require(`../images/logo.png`)}
-        alt="mainImage"
-        className="img-fluid logo-image"
-      />
-    </div>
-  </section>
+    {data?
+    <section id="main" className="mx-auto mt-2 py-5 px-5">
+      <h3 className="mx-auto mb-4">전신갑주 입고 홀스 성회 가자</h3>
+        <div className="col-5 mx-auto col-md-5 col-sm-5 col-12">
+          <img  src={require(`../images/main2.png`)} className="img-fluid" alt="mainLogo" />
+        </div>
+      <p className="mt-3 mb-4">나에게 맞은 말씀의 검은?</p>
+      <button onClick={moveStart}>시작하기</button>
+      <div className="logo-container">
+        <img
+          src={require(`../images/logo.png`)}
+          alt="mainImage"
+          className="img-fluid logo-image"
+        />
+      </div>
+    </section>
+    :null}
   </div>
-
+ 
   );
 }
 
