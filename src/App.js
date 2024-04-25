c5b18f3f630791a810c60789fff230aa45bb55ad
sincera <spzspz@live.com>
2024-04-25 15:55:15 +0900

    STYLE : calculate 화면 레이아웃 구성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './component/Home';
+import Calculate from './component/Calculate';
 import './css/App.css';
 
 import Result from './component/Result'
@@ -15,6 +16,7 @@ const App = () => {
     return(
       <Routes>
       <Route path='' element={<Home/>}/>
+      <Route path='/calculate' element={<Calculate/>}/>
       <Route path='/searchResult'>
         <Route path=':search' element={<Result/>}/>
       </Route>
---
+++ src/component/Calculate.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+import '../css/main.css';
+import '../css/default.css';
+import '../css/calculate.css'
+import axios from 'axios';
+
+const Calculate = (props) => {
+
+  const [resultMember, setResultMember] = useState(0);
+  const [data , setData] = useState(false);
+
+
+  return (
+    <>
+    <div id='calculate' className='calculate-container'>
+    <div id='image' className='calculate-image-container'>
+    <img
+        src={require(`../images/q/q5.png`)}
+        alt="q1Image"
+        className={'img-fluid '}
+        style={{ width: "50%" }}
+      />
+    <p className='loading'>말씀의 검 만드는 중...</p>
+    </div>
+    </div>
+    </>
+ 
+  );
+}
+
+export default Calculate;
