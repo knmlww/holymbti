c9aaaa789fb42ac906c3193971af9b974d6dceea
sincera <spzspz@live.com>
2024-05-03 13:58:37 +0900

    REFACTOR : resultTemp 제거

--- src/component/Result.jsx
+++ src/component/Result.jsx
@@ -17,33 +17,35 @@ const Result = () => {
     const [resultMBTI, setResultMBTI] = useState("");
     const [resultCnt, setResultCnt] = useState(null);
     const [imgSrc, setImgSrc] = useState(null);
+    const [ccmImgSrc, setCcmImgSrc] = useState(null);
     const [issueNum, setIssueNum] = useState(null);
+    const [ccmUrl, setCcmUrl] = useState(null);
+    const [kakaoUrl, setKakaoUrl] = useState(null);
+
+    const [bible, setBible] = useState(null);
 
     const [searchParams, setSearchParams] = useSearchParams();
-
-    const [isKakaoBrower, setKakaoBrower] = useState(false);
-
 
     const { search } = useParams();
 
     useEffect(()=>{
-
-      const isKakao = navigator.userAgent.match("KAKAOTALK"); 
-      console.log(navigator.userAgent); 
-      setKakaoBrower(Boolean(isKakao)); 
-
-        const url = `/searchResult/${search}`;
+        const url = `/holymbti/searchResult/${search}`;
         axios.get(url, {
           params: {
             search: searchParams.get("search")
           }
         })
          .then((res)=>{
- 
+          console.dir(res.data.typeThumbnailImageUrl)
           setResultCnt(res.data.mbtiCount);
-          setResultMBTI(res.data.mbtiResult);
-          setImgSrc(res.data.imgName);
+          setResultMBTI(res.data.typeDtlName);
+          setImgSrc(res.data.typeImgUrl);
           setIssueNum(res.data.issueNum);
+          setResultCnt(res.data.mbtiCount);
+          setBible(res.data.typePray)
+          setCcmUrl(res.data.typeCcmUrl)
+          setCcmImgSrc(res.data.typeCcmImgUrl);
+          setKakaoUrl(res.data.typeThumbnailImageUrl);
           setData(true)
          }).catch((error)=>{
           setData(false);
@@ -52,70 +54,126 @@ const Result = () => {
     },[]);
       
     const moveHome = () => {
+        dispatch({type:"CLEAR_SCORE"})
         navigate('/')
     }
 
-
     const moveInstagram = (url) => {
-      if(isKakaoBrower){
-        window.open('kakaotalk://web/openExternal?url='+url, "_blank", "noopener, noreferrer");
-      }else{
-        window.open(url, "_blank", "noopener, noreferrer");
-      }
-
-  }
-
-  const getBrowser = () => {
-    const browsers = [
-      'Chrome', 'Opera',
-      'WebTV', 'Whale',
-      'Beonex', 'Chimera',
-      'NetPositive', 'Phoenix',
-      'Firefox', 'Safari',
-      'SkipStone', 'Netscape', 'Mozilla',
-    ];
-  
-    const userAgent = window.navigator.userAgent.toLowerCase();
-  
-    if (userAgent.includes("edg")) {
-      return "Edge";
-    }
-  
-    if (userAgent.includes("trident") || userAgent.includes("msie")) {
-      return "Internet Explorer";
-    }
-  
-    return browsers.find((browser) => userAgent.includes(browser.toLowerCase())) || 'Other';
+      window.open(url, "_blank", "noopener, noreferrer");
   }
 
   const handleCopyClipBoard = async (text) => {
     try {
-      await navigator.clipboard.writeText(text);
+            await navigator.clipboard.writeText(text);
       alert("클립보드에 링크가 복사되었어요.");
     } catch (err) {
-      console.log(err);
     }
   };
+
+ 
+  async function downloadFile  () {
+    
+    var useragt = navigator.userAgent.toLowerCase();
+		var target_url = "https://www.holymbti.kro.kr";
+		
+		if(useragt.match(/kakaotalk/i)){
+      fetch(imgSrc, { method: 'GET' })
+      .then((res) => {
+          return res.blob();
+      })
+      .then((blob) => {  
+        if (navigator.share) {
+          navigator.share({
+          //  title: 'WebShare API Demo',
+          //  url: 'https://codepen.io/ayoisaiah/pen/YbNazJ',
+          files: [
+            new File([blob], 'file.png', {
+              type: blob.type,
+            }),
+          ],
+          }).then(() => {
+            console.log('Thanks for sharing!');
+          })
+          .catch(console.error);
+        } else {
+
+          blobToBase64(blob).then(res => {
+            // do what you wanna do
+          const a = document.createElement('a');
+          a.href = res;
+          a.download = "img";
+          a.target="_blank"
+          a.filename ="img"
+          document.body.appendChild(a);
+          a.click();
+          setTimeout((_) => {
+              window.URL.revokeObjectURL(url);
+          }, 60000);
+          a.remove(             );
+
+          });
+
+      
+        }               
+      })
+   
+		}else{
+  
+    fetch(imgSrc, { method: 'GET' })
+        .then((res) => {
+            return res.blob();
+        })
+        .then((blob) => {         
+
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = "말씀의 검";
+            document.body.appendChild(a);
+            a.click();
+            setTimeout((_) => {
+                window.URL.revokeObjectURL(url);
+            }, 60000);
+            a.remove(             );
+
+ 
+        })
+        .catch((err) => {
+            console.error('err: ', err);
+        });
+      
+    } 
+
+  
+};
+
+function blobToBase64(blob) {
+  return new Promise((resolve, _) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.readAsDataURL(blob);
+  });
+}
 
   const shareKakao = () => {
     const imageUrl = document.getElementById("resultImage").src;
     
     const resultUrl = `https://www.holymbti.kro.kr/searchResult/${issueNum}`;
+
     if (window.Kakao) {
       const kakao = window.Kakao;
  
       if (!kakao.isInitialized()) {
         kakao.init(process.env.REACT_APP_KAKAO_KEY);
       }
-      console.log(Kakao);
       Kakao.Share.sendDefault({
+        
         objectType: 'feed',
         content: {
-          title: '나에게 필요한 말씀의 검은?',
-          description: '테스트 입니다. 문구 추천 바랍니다',
-          imageUrl:
-           // 'http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-           imageUrl,
+          title: '6월 1일, 부흥을 위한 성령의 검',
+          description: '마귀의 간계를 능히 대적하기 위하여 하나님의 전신갑주를 입으라',
+          imageUrl:kakaoUrl,
+          imageWidth:800,
           link: {
             // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
             mobileWebUrl: resultUrl,
@@ -145,39 +203,69 @@ const Result = () => {
 
     return (
         
-      <div id="final" className="final">
-          <img
-          src={require(`../images/p-bg/phoneBG.jpg`)}
-          alt="phoneBG"
-          className="img-fluid"
-          style={{ width: "100%" }}
-        />
-                <button className="mx-auto mt-4" >이미지 다운로드</button>
-      <h3 className="mx-auto mb-4 mt-5">나에게 필요한 말씀의 검은?</h3>
-      <h2>사랑에는 거짓이 없다니 악을 미워하고 선에 속하라 로마서 12:9</h2>
-      <section id="main" className="mx-auto">
-        <section className="mx-auto" />
-      
-      </section>
-      <div id="link">
-        <button idclass="mt-3"onClick={() => moveInstagram("https://www.instagram.com/theholyspirit_fg")}>insta</button>
-        <br />
-        <button className="mx-auto mt-1">main</button>
-      </div>
-      <div id="link2" className="mt-1">
-        <button className="mt-1">Link</button>
-        <button className="mx-auto mt-1">KaKao</button>
-      </div>
-      <div className="logo-container">
-        <img
-          src="./images/logo.png"
-          alt="mainImage"
-          className="img-fluid logo-image"
-        />
-      </div>
-    </div>
+      <div className="final-container">
+        {data?
+        <>
+        <div className='bible-section'>
+          <img id="resultTop" className='img-fluid' src={require(`../images/resultTop.png`)} alt="resultTop"/>
+
+            <p className='bible'>{bible}</p> 
+        </div>
+     
+        <div className="mbti-result">
+          <img id="resultImage" className='img-fluid' style={{width:"431px"}}	src={imgSrc} alt={resultMBTI}/>
+            <p className='bible-sword'>말씀 배경화면으로 전신갑주 완전무장!</p>
+        </div>
+        <div className='download-container'>
+        <button  className="download-button" onClick={downloadFile}
+        >
+        이미지 다운로드</button>
+        </div>
+          <div className='count-container'>
+            <div className='countBox'>
+              <p className='count-section'>나와 같은 검을 가진 사람의 수<span className='mbti-count'>{resultCnt}명</span></p>        
+            </div>
+          </div>
+
+         
+        <div className='ccm-img-container'>
+        <img id="ccmImage" className='img-fluid ccm-img'	src={ccmImgSrc} alt='ccm'/>
+        </div>
+
+        <button className="download-button" onClick={() => moveInstagram(ccmUrl)}>
+        들으러 가기</button>
+        <div className='last-button-container'>
+        <button className="last-button"  onClick={() => moveInstagram("http://www.youthfg.com/since/1")}>홀스 홈페이지 바로가기</button>  
+        <br/> 
+        <button className="last-button-instagram" onClick={() => moveInstagram("https://www.instagram.com/theholyspirit_fg")}>홀스 인스타 바로가기</button>  
+        <br/>
+        <button className="last-button" onClick={() => moveHome("https://www.instagram.com/theholyspirit_fg")}>테스트 다시하기</button>  
+        </div>
+
+
+        <div className='share-box'>
+        <div className='share'>
+        <img src={require(`../images/link-button.png`)} alt="result-logo" onClick={()=>handleCopyClipBoard(`${location}`)} className="img-fluid" style={{ width: "20%" }}/>
+        <img  src={require(`../images/kakao.png`)}
+            alt="카카오톡 공유 보내기 버튼"
+            className="img-fluid"
+            onClick={shareKakao}
+            style={{ width: "20%" }}/>
+        </div>
+        </div>
     
-      
+        </>
+         :null
+         }
+
+        <div id="logo" className='logo-container'>
+          <img
+            src={require(`../images/logo.png`)}
+            alt="result-logo"
+            className="img-fluid"
+          />
+        </div>
+      </div>
     );
   };
 
